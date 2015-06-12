08999860d5d78209f4e030cff9fe13532ecbae63
Vita_Chukhno <vita_chukhno@epam.com>
2015-06-12 18:04:53 +0300

    Added ng-class

--- Module1/hw1/angular-phonecat/app/js/controllers.js
+++ Module1/hw1/angular-phonecat/app/js/controllers.js
@@ -10,28 +10,39 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
     $scope.orderProp = 'age';
 
     $scope.wishList = [];
-    $scope.addToWishList = function (phoneId) {
-      $scope.wishList.push(phoneId);
-    }
+    $scope.addToWishList = function (phoneName) {
+      $scope.wishList.push(phoneName);
+        angular.forEach($scope.phones, function(phone) {
+            if(phoneName == phone.name) {
+                phone.isInWishList = true;
+            }
+        });
+    };
 
-    $scope.removeFromWishList = function(phoneId) {
-      var index = $scope.wishList.indexOf(phoneId);
+    $scope.removeFromWishList = function(phoneName) {
+      var index = $scope.wishList.indexOf(phoneName);
       $scope.wishList.splice(index, 1);
-    }
+        angular.forEach($scope.phones, function(phone) {
+            if(phoneName == phone.name) {
+                phone.isInWishList = false;
+            }
+        });
+        console.log(phone.name);
+    };
 
-    $scope.isInWishList = function(phoneId) {
-      var index = $scope.wishList.indexOf(phoneId);
+    $scope.isInWishList = function(phoneName) {
+      var index = $scope.wishList.indexOf(phoneName);
       if (index != -1) {
-        $scope.removeFromWishList(phoneId);
+        $scope.removeFromWishList(phoneName);
       } else {
-        $scope.addToWishList(phoneId);
+        $scope.addToWishList(phoneName);
       }
     }
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
   function ($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
+    $scope.phone = Phone.get({phoneName: $routeParams.phoneName}, function (phone) {
       $scope.mainImageUrl = phone.images[0];
     });
 
